89666d3224eef917416ae0e5a1692c235b7a9e75
fericho029 <fericho029@gmail.com>
2025-09-26 18:36:53 +0300

    Task 2, Laba 1, print

--- Laba-1/Task 2.py
+++ Laba-1/Task 2.py
@@ -2,4 +2,4 @@ text = input("Введите строку:");
 letters = "aeiouAEIOU";
 
 result = ''.join(char for char in text if char not in letters);
-print(text, "\n", result);
+print(f"Исходный текст: {text} ""\n"f"Итоговый текст: {result}");
