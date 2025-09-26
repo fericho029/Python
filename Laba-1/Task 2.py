13f81e47b44cd7ef04d7c9945a6803cbde6b4a09
fericho029 <fericho029@gmail.com>
2025-09-26 18:56:33 +0300

    вывод в таск 1

--- Laba-1/Task 2.py
+++ Laba-1/Task 2.py
@@ -2,4 +2,4 @@ text = input("Введите строку:");
 letters = "aeiouAEIOU";
 
 result = ''.join(char for char in text if char not in letters);
-print(f"Исходный текст: {text} ""\n"f"Итоговый текст: {result}");
+print(f"Исходный текст: {text} ""\n"f"Итоговый текст без символов: {result}");
