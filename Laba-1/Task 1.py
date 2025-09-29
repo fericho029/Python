32f97e7b64c6290bf7b87e222acd4c02228caa64
fericho029 <fericho029@gmail.com>
2025-09-29 23:21:29 +0300

    Revert "Task 1, add initials"
    This reverts commit 6d9afbd25b0479a3e6745c395c4ab5e5711b66a6.

--- Laba-1/Task 1.py
+++ Laba-1/Task 1.py
@@ -1,6 +1,5 @@
-Name = input("Name: ");
-Surname = input("Surname: ");
-SecName = input("SecName: ");
+Name = input("Name");
+Surname = input("Surname");
+SecName = input("SecName");
 
-initials = Surname[0] + ". " + SecName[0] + ".";
-print ("Ваше ФИО: ", Name, initials);
+print ("Ваше ФИО: ", Name, Surname, SecName);
